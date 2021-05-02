825ffbbc4211e0073d460822280bf50c06ba5627
as2811-project <83534298+as2811-project@users.noreply.github.com>
2021-05-02 16:32:47 +0530

    Update eda.py

--- apps/eda.py
+++ apps/eda.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.figure_factory as ff
-import matplotlib.pyplot as plt
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
